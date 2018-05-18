d1b6ea234fec0dc2ff6006981f540de5f331ec4b
Maximilien Mellen <maxmellen0@gmail.com>
2018-05-18 17:54:06 +0200

    Process Elm with elm-webpack-loader

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -176,6 +176,16 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          // Process Elm with elm-webpack-loader.
+          {
+            test: /\.elm$/,
+            include: paths.appSrc,
+            loader: require.resolve('elm-webpack-loader'),
+            options: {
+              warn: true,
+              debug: true
+            },
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
@@ -223,7 +233,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.elm$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -180,6 +180,13 @@ module.exports = {
               compact: true,
             },
           },
+          // Process Elm with elm-webpack-loader.
+          {
+            test: /\.elm$/,
+            include: paths.appSrc,
+            loader: require.resolve('elm-webpack-loader'),
+            options: {},
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
@@ -249,7 +256,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.elm$/, /\.html$/, /\.json$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
